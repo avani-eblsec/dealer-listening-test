cd9f4ef9db4e2a23077677a63a485fbe8809846e
Avani Singh <82566636+avani-eblsec@users.noreply.github.com>
2026-05-20 13:49:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -574,3 +574,25 @@ function showResults(){
         </div>
     `;
 }
+
+document.addEventListener(
+    'contextmenu',
+    e => e.preventDefault()
+);
+
+document.onkeydown = function(e){
+
+    if(e.keyCode == 123){
+
+        return false;
+    }
+
+    if(
+        e.ctrlKey &&
+        e.shiftKey &&
+        e.keyCode == 'I'.charCodeAt(0)
+    ){
+
+        return false;
+    }
+};
